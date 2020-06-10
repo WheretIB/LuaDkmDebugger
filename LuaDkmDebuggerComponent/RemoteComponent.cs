WheretIB/LuaDkmDebugger

commit 837532a82e6427ecd040268d5312b1c8a4852476

Lua hooks are set only when there are active breakpoints or Lua code stepping was used

LuaDkmDebuggerComponent/RemoteComponent.cs
@@ -47,6 +47,10 @@ namespace LuaDkmDebuggerComponent
         public DkmStepper activeStepper = null;
 
         public Dictionary<ulong, LuaFunctionData> functionDataCache = new Dictionary<ulong, LuaFunctionData>();
+
+        public Dictionary<ulong, RegisterStateMessage> knownStates = new Dictionary<ulong, RegisterStateMessage>();
+        public bool hooksEnabled = false;
+        public bool hadActiveStepper = false;
     }
 
     public class RemoteComponent : IDkmCustomMessageForwardReceiver, IDkmRuntimeBreakpointReceived, IDkmRuntimeMonitorBreakpointHandler, IDkmRuntimeStepper, IDkmLanguageConditionEvaluator, IDkmExceptionFormatter
@@ -108,8 +112,75 @@ namespace LuaDkmDebuggerComponent
             {
                 processData.luaVersion = (customMessage.Parameter1 as int?).GetValueOrDefault(0);
             }
+            else if (customMessage.MessageCode == MessageToRemote.registerLuaState)
+            {
+                var data = new RegisterStateMessage();
+
+                data.ReadFrom(customMessage.Parameter1 as byte[]);
+
+                Debug.Assert(processData.knownStates.ContainsKey(data.stateAddress) == false);
+
+                processData.knownStates.Add(data.stateAddress, data);
+
+                if (processData.hooksEnabled)
+                    SetupHooks(process, processData);
+            }
+            else if (customMessage.MessageCode == MessageToRemote.unregisterLuaState)
+            {
+                var data = new UnregisterStateMessage();
+
+                data.ReadFrom(customMessage.Parameter1 as byte[]);
+
+                Debug.Assert(processData.knownStates.ContainsKey(data.stateAddress) == true);
+
+                processData.knownStates.Remove(data.stateAddress);
+            }
 
             return null;
+        }
+
+        void SetupHooks(DkmProcess process, LuaRemoteProcessData processData)
+        {
+            processData.hooksEnabled = true;
+
+            foreach (var stateKV in processData.knownStates)
+            {
+                var state = stateKV.Value;
+
+                DebugHelpers.TryWritePointerVariable(process, state.hookFunctionAddress, state.helperHookFunctionAddress);
+                DebugHelpers.TryWriteIntVariable(process, state.hookMaskAddress, 7); // LUA_HOOKLINE | LUA_HOOKCALL | LUA_HOOKRET
+                DebugHelpers.TryWriteIntVariable(process, state.hookBaseCountAddress, 0);
+                DebugHelpers.TryWriteIntVariable(process, state.hookCountAddress, 0);
+            }
+        }
+
+        void RemoveHooks(DkmProcess process, LuaRemoteProcessData processData)
+        {
+            processData.hooksEnabled = false;
+
+            foreach (var stateKV in processData.knownStates)
+            {
+                var state = stateKV.Value;
+
+                DebugHelpers.TryWritePointerVariable(process, state.hookFunctionAddress, 0);
+                DebugHelpers.TryWriteIntVariable(process, state.hookMaskAddress, 0);
+                DebugHelpers.TryWriteIntVariable(process, state.hookBaseCountAddress, 0);
+                DebugHelpers.TryWriteIntVariable(process, state.hookCountAddress, 0);
+            }
+        }
+
+        void UpdateHooks(DkmProcess process, LuaRemoteProcessData processData)
+        {
+            if (processData.activeBreakpoints.Count != 0 || processData.hadActiveStepper)
+            {
+                if (!processData.hooksEnabled)
+                    SetupHooks(process, processData);
+            }
+            else
+            {
+                if (processData.hooksEnabled)
+                    RemoveHooks(process, processData);
+            }
         }
 
         void UpdateBreakpoints(DkmProcess process, LuaRemoteProcessData processData)
@@ -117,6 +188,8 @@ namespace LuaDkmDebuggerComponent
             // Can't update breakpoints if we don't have the hook attached
             if (processData.locations == null)
                 return;
+
+            UpdateHooks(process, processData);
 
             int count = processData.activeBreakpoints.Count;
 
@@ -422,6 +495,10 @@ namespace LuaDkmDebuggerComponent
             }
 
             processData.activeStepper = stepper;
+
+            processData.hadActiveStepper = true;
+
+            UpdateHooks(process, processData);
         }
 
         void IDkmRuntimeStepper.StopStep(DkmRuntimeInstance runtimeInstance, DkmStepper stepper)

LuaDkmDebuggerComponent/Guids.cs
@@ -27,6 +27,8 @@ namespace LuaDkmDebuggerComponent
         public static readonly int resumeBreakpoints = 4;
         public static readonly int luaVersionInfo = 5;
         public static readonly int throwException = 6;
+        public static readonly int registerLuaState = 7;
+        public static readonly int unregisterLuaState = 8;
     }
 
     static class MessageToLocal

LuaDkmDebuggerComponent/LocalComponent.cs
@@ -2426,24 +2426,24 @@ namespace LuaDkmDebuggerComponent
 
                             if (hookFunctionAddress.HasValue && hookBaseCountAddress.HasValue && hookCountAddress.HasValue && hookMaskAddress.HasValue)
                             {
-                                if (LuaHelpers.luaVersion == 501)
+                                var message = new RegisterStateMessage
                                 {
-                                    DebugHelpers.TryWritePointerVariable(process, hookFunctionAddress.Value, processData.helperHookFunctionAddress_5_1);
-                                    DebugHelpers.TryWriteByteVariable(process, hookMaskAddress.Value, 7); // LUA_HOOKLINE | LUA_HOOKCALL | LUA_HOOKRET
-                                }
-                                else if (LuaHelpers.luaVersion == 502)
-                                {
-                                    DebugHelpers.TryWritePointerVariable(process, hookFunctionAddress.Value, processData.helperHookFunctionAddress_5_2);
-                                    DebugHelpers.TryWriteByteVariable(process, hookMaskAddress.Value, 7); // LUA_HOOKLINE | LUA_HOOKCALL | LUA_HOOKRET
-                                }
-                                else if (LuaHelpers.luaVersion == 503)
-                                {
-                                    DebugHelpers.TryWritePointerVariable(process, hookFunctionAddress.Value, processData.helperHookFunctionAddress_5_3);
-                                    DebugHelpers.TryWriteIntVariable(process, hookMaskAddress.Value, 7); // LUA_HOOKLINE | LUA_HOOKCALL | LUA_HOOKRET
-                                }
+                                    stateAddress = stateAddress.Value,
 
-                                DebugHelpers.TryWriteIntVariable(process, hookBaseCountAddress.Value, 0);
-                                DebugHelpers.TryWriteIntVariable(process, hookCountAddress.Value, 0);
+                                    hookFunctionAddress = hookFunctionAddress.Value,
+                                    hookBaseCountAddress = hookBaseCountAddress.Value,
+                                    hookCountAddress = hookCountAddress.Value,
+                                    hookMaskAddress = hookMaskAddress.Value,
+                                };
+
+                                if (LuaHelpers.luaVersion == 501)
+                                    message.helperHookFunctionAddress = processData.helperHookFunctionAddress_5_1;
+                                else if (LuaHelpers.luaVersion == 502)
+                                    message.helperHookFunctionAddress = processData.helperHookFunctionAddress_5_2;
+                                else if (LuaHelpers.luaVersion == 503)
+                                    message.helperHookFunctionAddress = processData.helperHookFunctionAddress_5_3;
+
+                                DkmCustomMessage.Create(process.Connection, process, MessageToRemote.guid, MessageToRemote.registerLuaState, message.Encode(), null).SendLower();
 
                                 log.Debug("Hooked Lua state");
                             }
@@ -2477,6 +2477,13 @@ namespace LuaDkmDebuggerComponent
                         log.Debug($"Removing Lua state 0x{stateAddress:x} from symbol store");
 
                         processData.symbolStore.Remove(stateAddress.Value);
+
+                        var message = new UnregisterStateMessage
+                        {
+                            stateAddress = stateAddress.Value,
+                        };
+
+                        DkmCustomMessage.Create(process.Connection, process, MessageToRemote.guid, MessageToRemote.unregisterLuaState, message.Encode(), null).SendLower();
                     }
 
                     inspectionSession.Close();

LuaDkmDebuggerComponent/Messages.cs
@@ -144,4 +144,91 @@ namespace LuaDkmDebuggerComponent
             return true;
         }
     }
+
+    public class RegisterStateMessage
+    {
+        public ulong stateAddress = 0;
+
+        public ulong hookFunctionAddress = 0;
+        public ulong hookBaseCountAddress = 0;
+        public ulong hookCountAddress = 0;
+        public ulong hookMaskAddress = 0;
+
+        public ulong helperHookFunctionAddress = 0;
+
+        public byte[] Encode()
+        {
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new BinaryWriter(stream))
+                {
+                    writer.Write(stateAddress);
+
+                    writer.Write(hookFunctionAddress);
+                    writer.Write(hookBaseCountAddress);
+                    writer.Write(hookCountAddress);
+                    writer.Write(hookMaskAddress);
+
+                    writer.Write(helperHookFunctionAddress);
+
+                    writer.Flush();
+
+                    return stream.ToArray();
+                }
+            }
+        }
+
+        public bool ReadFrom(byte[] data)
+        {
+            using (var stream = new MemoryStream(data))
+            {
+                using (var reader = new BinaryReader(stream))
+                {
+                    stateAddress = reader.ReadUInt64();
+
+                    hookFunctionAddress = reader.ReadUInt64();
+                    hookBaseCountAddress = reader.ReadUInt64();
+                    hookCountAddress = reader.ReadUInt64();
+                    hookMaskAddress = reader.ReadUInt64();
+
+                    helperHookFunctionAddress = reader.ReadUInt64();
+                }
+            }
+
+            return true;
+        }
+    }
+
+    public class UnregisterStateMessage
+    {
+        public ulong stateAddress = 0;
+
+        public byte[] Encode()
+        {
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new BinaryWriter(stream))
+                {
+                    writer.Write(stateAddress);
+
+                    writer.Flush();
+
+                    return stream.ToArray();
+                }
+            }
+        }
+
+        public bool ReadFrom(byte[] data)
+        {
+            using (var stream = new MemoryStream(data))
+            {
+                using (var reader = new BinaryReader(stream))
+                {
+                    stateAddress = reader.ReadUInt64();
+                }
+            }
+
+            return true;
+        }
+    }
 }

commit 3deb8df1e07b2be0010515b00a13c01cd22efaf1

Fixed breakpoints and stepping in Lua 5.1 and 5.2 (broken by rewrite in 837532a82e6427ecd040268d5312b1c8a4852476)

LuaDkmDebuggerComponent/RemoteComponent.cs
@@ -148,7 +148,12 @@ namespace LuaDkmDebuggerComponent
                 var state = stateKV.Value;
 
                 DebugHelpers.TryWritePointerVariable(process, state.hookFunctionAddress, state.helperHookFunctionAddress);
-                DebugHelpers.TryWriteIntVariable(process, state.hookMaskAddress, 7); // LUA_HOOKLINE | LUA_HOOKCALL | LUA_HOOKRET
+
+                if (processData.luaVersion == 503)
+                    DebugHelpers.TryWriteIntVariable(process, state.hookMaskAddress, 7); // LUA_HOOKLINE | LUA_HOOKCALL | LUA_HOOKRET
+                else
+                    DebugHelpers.TryWriteByteVariable(process, state.hookMaskAddress, 7); // LUA_HOOKLINE | LUA_HOOKCALL | LUA_HOOKRET
+
                 DebugHelpers.TryWriteIntVariable(process, state.hookBaseCountAddress, 0);
                 DebugHelpers.TryWriteIntVariable(process, state.hookCountAddress, 0);
             }
@@ -163,7 +168,12 @@ namespace LuaDkmDebuggerComponent
                 var state = stateKV.Value;
 
                 DebugHelpers.TryWritePointerVariable(process, state.hookFunctionAddress, 0);
-                DebugHelpers.TryWriteIntVariable(process, state.hookMaskAddress, 0);
+
+                if (processData.luaVersion == 503)
+                    DebugHelpers.TryWriteIntVariable(process, state.hookMaskAddress, 0);
+                else
+                    DebugHelpers.TryWriteByteVariable(process, state.hookMaskAddress, 0);
+
                 DebugHelpers.TryWriteIntVariable(process, state.hookBaseCountAddress, 0);
                 DebugHelpers.TryWriteIntVariable(process, state.hookCountAddress, 0);
             }